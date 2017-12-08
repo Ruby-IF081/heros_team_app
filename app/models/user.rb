6d191a684eacbc482e95608fc84ea32a751de9b3
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2017-12-08 15:20:15 +0200

    Added devise model User and migration for it

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  enum role: %i[sale admin moderator]
+  before_save { email.downcase! }
+  validates :first_name,
+            presence: true,
+            length: { minimum: 3, maximum: 50 }
+  validates :last_name,
+            presence: true,
+            length: { minimum: 3, maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
