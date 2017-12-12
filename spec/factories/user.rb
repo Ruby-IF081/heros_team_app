d655145ce4ef8f37e7413c72c417a0b2989f9282
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2017-12-12 15:42:14 +0200

    Refactor after reviewing and change default role for users

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
+  sequence :email do |n|
+    "#{n}#{Faker::Internet.email}"
+  end
   factory :user, class: User do
-    first_name { Faker::Name.last_name }
-    last_name { Faker::Name.last_name }
-    email { Faker::Internet.safe_email }
-    password { '1Qaz2Wsx' }
+    first_name { Faker::Name.unique.first_name }
+    last_name { Faker::Name.unique.last_name }
+    email { generate :email }
+    password { Faker::Internet.password }
   end
 end
