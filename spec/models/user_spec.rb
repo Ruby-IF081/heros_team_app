f6b141dd6b140937fb7f76f2db4fa3a696a9bd15
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2017-12-12 15:42:14 +0200

    Refactor after reviewing and change default role for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-  it 'is valid with first name, last name, e-mail and password' do
-    user = FactoryBot.build(:user)
-    expect(user).to be_valid
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
   end
-
-  it 'is invalid without first name' do
-    user = FactoryBot.build(:user, first_name: nil)
-    user.valid?
-    expect(user.errors[:first_name]).to include("can't be blank")
-  end
-
-  it 'is invalid without last name' do
-    user = FactoryBot.build(:user, last_name: nil)
-    user.valid?
-    expect(user.errors[:last_name]).to include("can't be blank")
-  end
-
-  it 'is invalid without e-mail' do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
-
-  it 'is invalid with duplicate e-mail' do
-    first_user = FactoryBot.build(:user, email: 'example@gmail.com')
-    first_user.save
-    second_user = FactoryBot.build(:user, email: 'example@gmail.com')
-    second_user.valid?
-    expect(second_user.errors[:email]).to include('has already been taken')
-  end
-
 end
