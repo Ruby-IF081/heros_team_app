083db63b12996f7a4dcc7ac097546809538aae6c
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2017-12-13 17:37:49 +0200

    Delete useless views and updating validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,14 @@ class User < ApplicationRecord
   ROLES = { sale: 0, admin: 1, moderator: 2 }.freeze
   enum role: ROLES
   before_save { email.downcase! }
-  validates :first_name,
-            presence: true,
-            length: { minimum: 3, maximum: 50 }
-  validates :last_name,
-            presence: true,
-            length: { minimum: 3, maximum: 50 }
+  validates :first_name, length: { minimum: 3, maximum: 50 }, presence: true
+  validates :last_name,  length: { minimum: 3, maximum: 50 }, presence: true
+  validates :password,   length: { minimum: 6, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email,
-            presence: true,
-            length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
-
+  validates :email, length: { maximum: 255 },
+                    presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
