f11c170d8c3a63a85a7284794ee56d8234caff97
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2017-12-14 09:46:14 +0200

    Delete validation tests and add factory test

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :user, class: User do
-    sequence(:email) { |n| "#{n}#{Faker::Internet.email}" }
     first_name { Faker::Name.unique.first_name }
     last_name { Faker::Name.unique.last_name }
-    email { generate :email }
+    sequence(:email) { |n| "#{n}#{Faker::Internet.email}" }
     password { Faker::Internet.password }
   end
 end
