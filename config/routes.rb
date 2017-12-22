57cdf349039bd6c354aa995a442d2d284ba19f7b
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2017-12-22 15:33:41 +0200

    Add custom fields for registration user, change role type and default role for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'account', controllers: {
+    registrations: 'users/registrations'
+  }
 
   namespace :account do
     resources :pages, only: %i[show index]
