3e8366995c47eaa86f98bfc8d906723c8f004cac
Misha7776 <vmisha7776@gmail.com>
2017-12-22 23:06:08 +0200

    Fixed Rubocop Offenses

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,23 +1,34 @@
 module DeviseHelper
   def devise_error_messages!
     return '' if resource.errors.empty?
+    html = html_var
+    html.html_safe
+  end
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t('errors.messages.not_saved',
-                      count: resource.errors.count,
-                      resource: resource.class.model_name.human.downcase)
+  private
 
-    html = <<-HTML
+  def error_messages
+    resource.errors.full_messages.map do |msg|
+      content_tag(:li, msg)
+    end.join
+  end
+
+  def error_sentence
+    I18n.t('errors.messages.not_saved',
+           count: resource.errors.count,
+           resource: resource.class.model_name.human.downcase)
+  end
+
+  def html_var
+    <<-HTML
     <div class="alert alert-danger alert-dismissible mb-2" role="alert">
        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
         </button>
         <h5 class="alert-heading mb-2">Warning!</h5>
-        <p>#{sentence}</p>
-           #{messages}
+        <p>#{error_sentence}</p>
+           #{error_messages}
     </div>
     HTML
-
-    html.html_safe
   end
 end
