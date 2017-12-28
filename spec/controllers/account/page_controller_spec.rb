932504819f6f1b76ed1d7a8fde7946209061d492
Serhii.Bodnaruk <sergiwez@gmail.com>
2017-12-28 14:43:07 +0200

    Some changes

--- spec/controllers/account/page_controller_spec.rb
+++ spec/controllers/account/page_controller_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe Account::PagesController, type: :controller do
     before(:each) { sign_in page.company.user }
 
     it 'GET #index' do
-      get :index, params: { company_id: page.company }
+      get :index, params: { company_id: page.company.to_param }
       expect(response).to have_http_status(:success)
     end
 
     it 'GET #show' do
-      get :index, params: { company_id: page.company, id: page.id }
+      get :index, params: { company_id: page.company.to_param, id: page.id }
       expect(response).to have_http_status(:success)
     end
   end
