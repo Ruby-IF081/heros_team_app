2ebe957e2605ffeb47745e96b407842babd7c828
egorodessa <egor.odessa@gmail.com>
2018-01-03 18:53:53 +0200

    need help: test do not run, and apicode invalid during tests, but not at usual use

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,6 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  WebMock.disable_net_connect!(allow_localhost: true)
 end
