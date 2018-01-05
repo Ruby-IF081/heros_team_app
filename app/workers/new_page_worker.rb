1500b227940cb555e713900bfdef04429c6302df
Serhii.Bodnaruk <sergiwez@gmail.com>
2018-01-05 22:12:08 +0200

    Refactored new page worker

--- app/workers/new_page_worker.rb
+++ app/workers/new_page_worker.rb
@@ -3,27 +3,32 @@ class NewPageWorker
 
   def perform(page_id)
     page = Page.find(page_id)
+    page.update_attributes(status: Page::ACTIVE_STATUS)
     make_screenshot(page)
     parse_html_content(page)
-    puts "Page #{page.id} created!"
+    logger.info "Page #{page.id} processed!"
   end
 
   private
 
+  def downloading_screenshot(file, source_url)
+    Gastly.capture(source_url, file.path)
+  end
+
   def make_screenshot(page)
-    image_path_to_save = Rails.root.join('tmp', "page_shot_#{page.id}.png")
-    image = Gastly.capture(page.source_url, image_path_to_save)
-    page.update_attributes(status: Page::ACTIVE_STATUS, screenshot: File.new(File.join(image)))
-    File.delete(image)
+    file = Tempfile.new
+    page.update_attributes(screenshot: File.new(downloading_screenshot(file, page.source_url)))
+    file.unlink
+  end
+
+  def downloading_content(source_url)
+    Nokogiri::HTML(open(source_url))
   end
 
   def parse_html_content(page)
-    doc_html = Nokogiri::HTML(open(page.source_url))
+    doc_html = downloading_content(page.source_url)
     title = doc_html.css('title').first.content
-    doc = ''
-    doc_html.css('body').each do |tag|
-      doc += tag.content.to_s
-    end
+    doc = doc_html.css('body').first.content
     page.update_attributes(content_html: doc_html, content: doc, title: title)
   end
 end
