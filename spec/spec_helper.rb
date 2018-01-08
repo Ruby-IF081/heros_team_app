245d9d91db7ae08201607af37a187d9de986b6a7
egorodessa <egor.odessa@gmail.com>
2018-01-08 23:25:55 +0200

    rebased

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-require 'webmock/rspec'
-
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -9,6 +7,4 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  WebMock.disable_net_connect!(allow_localhost: true)
 end
