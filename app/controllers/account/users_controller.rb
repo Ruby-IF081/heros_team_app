571d464393e637428178be43d22683a80f4b6a70
Misha7776 <vmisha7776@gmail.com>
2018-01-10 20:02:36 +0200

    Fixed breadcrumbs helper code, added it to other views

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -19,7 +19,6 @@ class Account::UsersController < ApplicationController
 
   def show
     @user = resource
-    @breadcrumbs_hash = resource_breadcrumbs
   end
 
   def new
@@ -70,10 +69,5 @@ class Account::UsersController < ApplicationController
 
   def resource
     collection.find(params[:id])
-  end
-
-  def resource_breadcrumbs
-    { 'All Users' => account_users_path,
-      @user.full_name => account_users_path(@user) }.freeze
   end
 end
--- app/helpers/breadcrumbs_helper.rb
+++ app/helpers/breadcrumbs_helper.rb
@@ -1,9 +1,5 @@
 module BreadcrumbsHelper
   def render_breadcrumbs(breadcrumbs_hash)
-    @breadcrumbs ||= []
-    breadcrumbs_hash.each do |title, path|
-      @breadcrumbs << { title: title, path: path }
-    end
-    render partial: 'account/shared/breadcrumbs', locals: { nav: @breadcrumbs }
+    render partial: 'account/shared/breadcrumbs', locals: { nav: breadcrumbs_hash }
   end
 end
