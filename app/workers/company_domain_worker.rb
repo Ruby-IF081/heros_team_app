37e1d4981a3c69daed6225003136e40f0fbf2a1e
chu <antonchupack@gmail.com>
2018-01-13 19:01:39 +0200

    Applied comment, added spec for invalid data

--- app/workers/company_domain_worker.rb
+++ app/workers/company_domain_worker.rb
@@ -1,43 +1,41 @@
 class CompanyDomainWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
-  attr_reader :company, :filt_links, :domain
+  attr_reader :company, :domain
 
   def perform(company_id)
     @company = Company.find(company_id)
     @domain = @company.domain
 
-    page_create_pending(@domain)
+    create_pending_page(domain)
     sub_pages_process
   end
 
   private
 
   def sub_pages_process
-    @filt_links = filtered_sub_pages_links
-    @filt_links.each do |link|
-      page_create_pending(link)
+    filtered_sub_pages_links.each do |link|
+      create_pending_page(link)
     end
   end
 
   def link_open(link)
-    link = 'http://' + link if link.exclude? '://'
+    link = 'http://' + link if link.exclude?('://')
     Nokogiri::HTML(open(link))
   end
 
   def page_html_links
-    domain_index = link_open(@domain)
+    domain_index = link_open(domain)
     main_html = domain_index.css('body')
     main_html.css('a').map { |href_link| href_link['href'] }
   end
 
   def filtered_sub_pages_links
-    links = page_html_links
-    links = links.uniq.reject { |page_link| page_link.to_s.empty? }
+    links = page_html_links.uniq.reject { |page_link| page_link.to_s.empty? }
     sub_links = []
     links.each do |link|
       next if link == '#'
-      next if link.include?('://') && link.exclude?(@domain)
+      next if link.include?('://') && link.exclude?(domain)
       link = page_url(link)
       sub_links.push(link)
     end
@@ -45,15 +43,14 @@ class CompanyDomainWorker
   end
 
   def page_url(link)
-    return (@domain + link) if link.exclude? '://'
+    return (domain + link) if link.exclude?('://')
     link
   end
 
-  def page_create_pending(source_url)
-    Page.create(title: Page::PENDING_TITLE,
-                page_type: Page::OFFICIAL_PAGE,
-                source_url: source_url,
-                status: Page::PENDING_STATUS,
-                company: @company)
+  def create_pending_page(source_url)
+    company.pages.create(title: Page::PENDING_TITLE,
+                         page_type: Page::OFFICIAL_PAGE,
+                         source_url: source_url,
+                         status: Page::PENDING_STATUS)
   end
 end
--- spec/workers/company_domain_worker_spec.rb
+++ spec/workers/company_domain_worker_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe CompanyDomainWorker, sidekiq: true do
   let!(:company) { create(:company, :worker_domain) }
   let!(:worker) { CompanyDomainWorker.new }
-  let!(:response_html) { Nokogiri::HTML(open('spec/factories/test.html')) }
 
   before(:each) do
     allow(worker).to receive(:link_open).and_return(response_html)
@@ -11,29 +10,49 @@ RSpec.describe CompanyDomainWorker, sidekiq: true do
     worker.perform(company.id)
   end
 
-  it 'testing worker queueing' do
-    expect { CompanyDomainWorker.perform_async(1) }.to change(CompanyDomainWorker.jobs, :size).by(1)
-  end
+  describe 'with valid data' do
+    let!(:response_html) { Nokogiri::HTML(open('spec/factories/test.html')) }
 
-  it 'worker company should be as created one' do
-    expect(worker.company).to eq(company)
-  end
+    it 'testing worker queueing' do
+      expect { CompanyDomainWorker.perform_async(1) }.to change(CompanyDomainWorker.jobs, :size).by(1)
+    end
 
-  it 'sub_links content should be correct. not nil, empty, outside' do
-    worker.filt_links.each do |link|
-      expect(link).not_to be_nil
-      expect(link).not_to be_empty
-      expect(link).not_to be('#')
+    it 'worker company should be as created one' do
+      expect(worker.company).to eq(company)
+    end
 
-      expect(link).to include(worker.domain)
+    it 'sub_links content should be correct. not nil, empty, outside' do
+      worker.send(:filtered_sub_pages_links).each do |link|
+        expect(link).not_to be_nil
+        expect(link).not_to be_empty
+        expect(link).not_to be('#')
+
+        expect(link).to include(worker.domain)
+      end
+    end
+
+    it 'number of created pages should be as number of sub_links' do
+      expect { worker.perform(company.id) }.to change(Page, :count).by(89)
     end
   end
 
-  it 'number of created pages should be as number of sub_links' do
-    page_num = worker.filt_links.length + 1
-    # +1 because company_domain_url is not included
-    # in links we are getting from the page html itself
+  describe 'with invalid data' do
+    let!(:response_html) do
+      Nokogiri::HTML('<html><head><title>Hello World</title></head>
+                      <body><a href="#"></a>
+                            <a href=""></a>
+                            <a href=></a>
+                            <a href="http://youtube.com"></a></body></html>')
+    end
 
-    expect { worker.perform(company.id) }.to change(Page, :count).by(page_num)
+    it 'empty sub_links array' do
+      expect(worker.send(:filtered_sub_pages_links)).to be_empty
+    end
+
+    it 'number of created pages should be as number of sub_links' do
+      # 1 because worker still creates page with company_domain
+      # inserted by user while creating a company
+      expect { worker.perform(company.id) }.to change(Page, :count).by(1)
+    end
   end
 end
