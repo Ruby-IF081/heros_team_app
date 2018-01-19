522fdd0e5d1311a5c461dbd61f0a703857f90196
chu <antonchupack@gmail.com>
2018-01-19 14:59:01 +0200

    [+]Added default meta-tags to dry-out layouts

---
+++ app/helpers/meta_tags_helper.rb
@@ -0,0 +1,10 @@
+module MetaTagsHelper
+  def default_meta_tags
+    {
+      site: 'Heroes Team App',
+      title: 'Sales Assistant',
+      description: 'Best assistant in the search of customers',
+      reverse: true
+    }
+  end
+end
