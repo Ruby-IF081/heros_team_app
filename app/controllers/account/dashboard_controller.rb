4cd31dfa78faced3356462287dc34871a8208944
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2018-01-31 22:55:59 +0200

    Add showing number of new pages added from yesterday

--- app/controllers/account/dashboard_controller.rb
+++ app/controllers/account/dashboard_controller.rb
@@ -3,5 +3,18 @@ class Account::DashboardController < ApplicationController
 
   def index
     @chart_value = current_companies
+    @count_of_pages = todays_pages
+  end
+
+  private
+
+  def todays_pages
+    count = 0
+    current_companies.each do |company|
+      count += company.pages
+                      .where('created_at > ?', 1.day.ago)
+                      .where.not(page_type: Page::CHROME_EXTENSION).count
+    end
+    count
   end
 end
