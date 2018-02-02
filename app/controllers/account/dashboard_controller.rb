4f39c82549a4e50d87e3e525d850c5230586ca17
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2018-02-02 16:19:37 +0200

    Refactor after Roman's reviewing

--- app/controllers/account/dashboard_controller.rb
+++ app/controllers/account/dashboard_controller.rb
@@ -1,9 +1,11 @@
 class Account::DashboardController < ApplicationController
+  LIMIT_OF_RECENT_COMPANIES = 5
+
   before_action :authenticate_user!
 
   def index
     @chart_value = current_companies
     @count_of_pages = CompanyPagesAnalytics.count_pages(current_tenant)
-    @recent_companies = current_companies.limit(10)
+    @recent_companies = current_companies.order(created_at: :desc).limit(10)
   end
 end
