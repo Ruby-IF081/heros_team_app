c81817e908f2611400e10c465de071700a4fdb07
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2018-02-05 16:27:36 +0200

    Refactor after reviewing

--- app/lib/company_pages_analytics.rb
+++ app/lib/company_pages_analytics.rb
@@ -1,9 +1,8 @@
 class CompanyPagesAnalytics
-  MANUALLY_ADDED_PAGES_TYPES = [Page::CHROME_EXTENSION, Page::BY_AJAX].freeze
   def self.count_pages(current_tenant)
     current_tenant.pages.where(
       'page_type not in (?) and pages.created_at > ?',
-      MANUALLY_ADDED_PAGES_TYPES, 1.day.ago
+      Page::MANUALLY_ADDED_PAGES_TYPES, 1.day.ago
     ).count
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -30,7 +30,7 @@ class Page < ApplicationRecord
   LINKEDIN_TYPE    = 'linkedin'.freeze
   CRUNCHBASE_TYPE  = 'crunchbase'.freeze
   CHROME_EXTENSION = 'chrome_extension'.freeze
-  BY_AJAX = 'by_ajax'.freeze
+  BY_AJAX          = 'by_ajax'.freeze
   OFFICIAL_PAGE    = 'official_page'.freeze
   PAGE_TYPES       = [BING_TYPE,
                       ANGLECO_TYPE,
@@ -39,6 +39,8 @@ class Page < ApplicationRecord
                       BY_AJAX,
                       CHROME_EXTENSION,
                       OFFICIAL_PAGE].freeze
+
+  MANUALLY_ADDED_PAGES_TYPES = [CHROME_EXTENSION, BY_AJAX].freeze
 
   LEGAL_RATING     = %w[1000 100 50 10 -10 -50 -100 -1000].freeze
 
