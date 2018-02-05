42296f2a6be9b02aa94ffd574707137f60638c67
Sviatoslav Krupa <sviatoslav.krupa@gmail.com>
2018-02-05 17:56:42 +0200

    Small correction

--- app/controllers/account/dashboard_controller.rb
+++ app/controllers/account/dashboard_controller.rb
@@ -7,8 +7,6 @@ class Account::DashboardController < ApplicationController
     @chart_value = current_companies
     @count_of_pages = CompanyPagesAnalytics.count_pages(current_tenant)
     @recent_companies = current_companies.recent.limit(LIMIT_TO_SHOW)
-    @recent_pages = current_tenant.pages.where(company_id: current_companies.pluck(:id))
-                                  .recent
-                                  .limit(LIMIT_TO_SHOW)
+    @recent_pages = current_tenant.pages.recent.limit(LIMIT_TO_SHOW)
   end
 end
