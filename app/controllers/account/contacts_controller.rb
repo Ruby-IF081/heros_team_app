7aff38fa043a453c9402729ee456ec3e7c73f96f
Oleksandr Adamchuk <oleks.adamchuk@gmail.com>
2018-02-05 16:45:06 +0200

    code style fix

--- app/controllers/account/contacts_controller.rb
+++ app/controllers/account/contacts_controller.rb
@@ -1,6 +1,6 @@
 class Account::ContactsController < ApplicationController
   before_action :authorize_super_admin!
-  
+
   def index
     @q = Contact.ransack(params[:q])
     @contacts = @q.result.page(params[:page]).per(20)
