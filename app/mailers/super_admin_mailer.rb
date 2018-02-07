7436c1d8ed89f0754c21743c9e9566f2250c46f1
chu <antonchupack@gmail.com>
2018-02-07 11:16:08 +0200

    Don't send emails if no super_admins

--- app/mailers/super_admin_mailer.rb
+++ app/mailers/super_admin_mailer.rb
@@ -1,8 +1,18 @@
 class SuperAdminMailer < ApplicationMailer
-  default to: proc { User.super_admins.pluck(:email) }
+  before_action :prevent_delivery_if_no_superadmins
 
   def daily_user_creation_notification
     @users = User.created_yesterday
-    mail subject: 'Users created on the previous day'
+    mail to: emails, subject: 'Users created on the previous day'
+  end
+
+  private
+
+  def prevent_delivery_if_no_superadmins
+    mail.perform_deliveries = false if emails.blank?
+  end
+
+  def emails
+    User.super_admins.pluck(:email)
   end
 end
