cbaf9546b111ab6b9450eb01522a11dde9b0bc29
chu <antonchupack@gmail.com>
2018-02-07 13:00:34 +0200

    Changed  superadmins existance check

--- app/mailers/super_admin_mailer.rb
+++ app/mailers/super_admin_mailer.rb
@@ -1,16 +1,10 @@
 class SuperAdminMailer < ApplicationMailer
-  before_action :prevent_delivery_if_no_superadmins
-
   def daily_user_creation_notification
     @users = User.created_yesterday
-    mail to: emails, subject: 'Users created on the previous day'
+    mail to: emails, subject: 'Users created on the previous day' if emails.any?
   end
 
   private
-
-  def prevent_delivery_if_no_superadmins
-    mail.perform_deliveries = false if emails.blank?
-  end
 
   def emails
     User.super_admins.pluck(:email)
