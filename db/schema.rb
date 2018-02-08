b613d79a6670d318b07ddb73a4682686ed81eb31
Misha7776 <vmisha7776@gmail.com>
2018-02-08 20:08:57 +0200

    Added mailgun gem and configured it for sending emails

--- db/schema.rb
+++ db/schema.rb
@@ -87,9 +87,7 @@ ActiveRecord::Schema.define(version: 20180203131713) do
     t.datetime "updated_at", null: false
     t.bigint "company_id"
     t.integer "rating", default: 0
-    t.datetime "deleted_at"
     t.index ["company_id"], name: "index_pages_on_company_id"
-    t.index ["deleted_at"], name: "index_pages_on_deleted_at"
   end
 
   create_table "tenants", force: :cascade do |t|
